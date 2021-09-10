95bd3949286772db9726d6e810b0346fc3462f27
Koonayoung518 <78741410+Koonayoung518@users.noreply.github.com>
2021-09-10 18:22:15 +0900

    last

---
+++ ting-master/src/main/java/com/ting/ting/TingApplication.java
@@ -0,0 +1,13 @@
+package com.ting.ting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TingApplication.class, args);
+	}
+
+}
---
+++ ting-master/src/main/java/com/ting/ting/controller/HomeController.java
@@ -0,0 +1,68 @@
+package com.ting.ting.controller;
+
+
+import com.ting.ting.model.User;
+import com.ting.ting.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping
+    public String index() {
+        return "index_ting";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+    @GetMapping("/callcenter")
+    public String callcenter(){
+        return "callcenter";
+    }
+
+
+    @GetMapping("/main")
+    public String main(){
+        return "main";
+    }
+    @GetMapping("/matchlist")
+    public String matchlist(){
+        return "matchlist";
+    }
+
+
+    @GetMapping("/notice_id")
+    public String notice_id(){
+        return "notice_id";
+    }
+    @GetMapping("/open")
+    public String open(){
+        return "open";
+    }
+    @GetMapping("/random")
+    public String random(){
+        return "random";
+    }
+    @GetMapping("/select")
+    public String select(){
+        return "select";
+    }
+    @GetMapping("/terms")
+    public String terms(){
+        return "terms";
+    }
+}
