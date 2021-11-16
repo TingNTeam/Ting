141348c559b5e2921aedc38bb52b986bb136c551
skfkgla <narahim.lee@gmail.com>
2021-11-16 15:37:19 +0900

    commonresponse

---
+++ ting-master/src/main/java/com/ting/ting/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.ting.ting.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    /**
+     * Bean Validation에 실패했을 때, 에러메시지를 내보내기 위한 Exception Handler
+     */
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ErrorResponse> handleParamViolationException(BindException ex) {
+        ErrorResponse response = ErrorResponse.builder()
+                .status(ErrorCode.REQUEST_PARAMETER_BIND_FAILED.getStatus().value())
+                .message(ErrorCode.REQUEST_PARAMETER_BIND_FAILED.getMessage())
+                .code(ErrorCode.REQUEST_PARAMETER_BIND_FAILED.getCode())
+                .build();
+        return new ResponseEntity<>(response, ErrorCode.REQUEST_PARAMETER_BIND_FAILED.getStatus());
+    }
+}
