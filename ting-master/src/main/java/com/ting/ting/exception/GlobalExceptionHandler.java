8255d83e8e5d487534af5b93f71eeed6a0aac1ef
skfkgla <narahim.lee@gmail.com>
2021-11-16 16:11:53 +0900

    commonresponse

--- ting-master/src/main/java/com/ting/ting/exception/GlobalExceptionHandler.java
+++ ting-master/src/main/java/com/ting/ting/exception/GlobalExceptionHandler.java
@@ -1,9 +1,15 @@
 package com.ting.ting.exception;
 
+import com.ting.ting.exception.errors.CustomJwtRuntimeException;
+import com.ting.ting.exception.errors.LoginFailedException;
+import com.ting.ting.exception.errors.RegisterFailedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,6 +18,7 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(BindException.class)
     protected ResponseEntity<ErrorResponse> handleParamViolationException(BindException ex) {
+        //클라이언트의 요청에 문제로 인한 예외
         ErrorResponse response = ErrorResponse.builder()
                 .status(ErrorCode.REQUEST_PARAMETER_BIND_FAILED.getStatus().value())
                 .message(ErrorCode.REQUEST_PARAMETER_BIND_FAILED.getMessage())
@@ -19,4 +26,73 @@ public class GlobalExceptionHandler {
                 .build();
         return new ResponseEntity<>(response, ErrorCode.REQUEST_PARAMETER_BIND_FAILED.getStatus());
     }
+    @ExceptionHandler(NoHandlerFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        // 없는 경로로 요청 시
+
+        ErrorResponse response = ErrorResponse.builder()
+                .message(ErrorCode.NOT_FOUND_PATH.getMessage())
+                .status(ErrorCode.NOT_FOUND_PATH.getStatus().value())
+                .code(ErrorCode.NOT_FOUND_PATH.getCode())
+                .build();
+
+        return new ResponseEntity<>(response, ErrorCode.NOT_FOUND_PATH.getStatus());
+    }
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
+        // GET POST 방식이 잘못된 경우
+
+        ErrorResponse response = ErrorResponse.builder()
+                .message(ErrorCode.METHOD_NOT_ALLOWED.getMessage())
+                .status(ErrorCode.METHOD_NOT_ALLOWED.getStatus().value())
+                .code(ErrorCode.METHOD_NOT_ALLOWED.getCode())
+                .build();
+
+        return new ResponseEntity<>(response, ErrorCode.METHOD_NOT_ALLOWED.getStatus());
+    }
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        //type이 잘못된 경우 (이 프로젝트의 경우 json)
+        ErrorResponse response = ErrorResponse.builder()
+                .message(ErrorCode.UNSUPPORTED_MEDIA_TYPE.getMessage())
+                .status(ErrorCode.UNSUPPORTED_MEDIA_TYPE.getStatus().value())
+                .code(ErrorCode.UNSUPPORTED_MEDIA_TYPE.getCode())
+                .build();
+
+        return new ResponseEntity<>(response, ErrorCode.UNSUPPORTED_MEDIA_TYPE.getStatus());
+    }
+    @ExceptionHandler(CustomJwtRuntimeException.class)
+    protected ResponseEntity<ErrorResponse> handleJwtException(CustomJwtRuntimeException e) {
+        //자바에서 제공하는 여러 JwtException 인증에 대한 오류일때(인증정보 부족, jwt토큰 만료등)
+        ErrorResponse response = ErrorResponse.builder()
+                .code(ErrorCode.INVALID_JWT_TOKEN.getCode())
+                .message(ErrorCode.INVALID_JWT_TOKEN.getMessage())
+                .status(ErrorCode.INVALID_JWT_TOKEN.getStatus().value())
+                .build();
+
+        return new ResponseEntity<>(response, ErrorCode.INVALID_JWT_TOKEN.getStatus());
+    }
+    @ExceptionHandler(RegisterFailedException.class)
+    protected ResponseEntity<ErrorResponse> handleRegisterFailedException(RegisterFailedException e) {
+
+        ErrorResponse response = ErrorResponse.builder()
+                .code(ErrorCode.REGISTER_FAILED.getCode())
+                .message(ErrorCode.REGISTER_FAILED.getMessage())
+                .status(ErrorCode.REGISTER_FAILED.getStatus().value())
+                .build();
+
+        return new ResponseEntity<>(response, ErrorCode.REGISTER_FAILED.getStatus());
+    }
+    @ExceptionHandler(LoginFailedException.class)
+    protected ResponseEntity<ErrorResponse> handleLoginFailedException(LoginFailedException e) {
+
+        ErrorResponse response = ErrorResponse.builder()
+                .code(ErrorCode.LOGIN_FAILED.getCode())
+                .message(ErrorCode.LOGIN_FAILED.getMessage())
+                .status(ErrorCode.LOGIN_FAILED.getStatus().value())
+                .build();
+
+        return new ResponseEntity<>(response, ErrorCode.LOGIN_FAILED.getStatus());
+    }
+
 }
